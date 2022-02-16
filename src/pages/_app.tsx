1968f82545ca9e1b0a549e722e2570abf72f347d
vieiramauricio <mauriciorenanvs@gmail.com>
2022-02-15 22:15:17 -0300

    :sparkles: added chakra and configured theme
    added chakra and configured theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,12 @@
 import { AppProps } from "next/app"
+import { ChakraProvider } from "@chakra-ui/react"
+import { theme } from "../styles/theme"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Component {...pageProps} />
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
   )
 }
 
